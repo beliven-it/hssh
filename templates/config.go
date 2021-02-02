8e696363b42de6bc8483ff50c33206ce7d59e476
Casval <fabrizio.gortani@protonmail.com>
2021-02-02 21:06:14 +0100

    refactor: use single string provider

--- templates/config.go
+++ templates/config.go
@@ -3,9 +3,5 @@ package templates
 // Config template
 var Config = `
 ssh_config_folder: ""
-default_provider: "gitlab"
-provider:
-  host: "https://gitlab.com/api/v4"
-  private_token: ""
-  project_id: ""
+provider: "gitlab://<TOKEN>:/<PROJECT_ID>@<FOLDER_PATH>"
 `
