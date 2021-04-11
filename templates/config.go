2a99d230b1f7c6a240d22077e24d7c08e0f221fd
Valentino Cossar <4087384+valentinocossar@users.noreply.github.com>
2021-04-11 22:19:10 +0200

    refactor: rewrite README and add example config file #6

--- templates/config.go
+++ templates/config.go
@@ -3,5 +3,5 @@ package templates
 // Config template
 var Config = `# HSSH configuration file
 providers:
-  - "gitlab://<TOKEN>:/<PROJECT_ID>@<FOLDER_PATH>"
+  - "<PROVIDER>://<ACCESS_TOKEN>:/<ENTITY_ID>@<SUBPATH>"
 `
