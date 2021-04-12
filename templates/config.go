e5dd02ab3de1a7d904c0c22036372aa9c641d853
Casval <fabrizio.gortani@protonmail.com>
2021-04-12 22:59:24 +0200

    refactor: change naming of config option

--- templates/config.go
+++ templates/config.go
@@ -2,7 +2,7 @@ package templates
 
 // Config template
 var Config = `# HSSH configuration file
-fuzzysearch_options: ""
+fzfsearch_options: ""
 providers:
   - "<PROVIDER>://<ACCESS_TOKEN>:/<ENTITY_ID>@<SUBPATH>"
 `
